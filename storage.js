2fda0c62a82c1c4bb784165be00aabce771ce961
tarungiridhar <tarungiri1237@gmail.com>
2023-09-16 19:45:01 -0400

    Storage update

--- storage.js
+++ storage.js
@@ -1,3 +1,11 @@
+function store() {
+    chrome.storage.local.set({ "firstName": document.getElementById("FirstName") }).then(() => {
+        console.log("First Name is set");
+      });
 
+    chrome.storage.local.get(["firstName"]).then((result) => {
+        console.log("Value currently is " + result.key);
+      });
+}
 
 console.log("This is a popup!")
