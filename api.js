2b83efcc151fb2e7779ede55fa7bc9f387ad9d12
tziffer <118021530+tziffer@users.noreply.github.com>
2023-09-17 01:50:15 -0400

    actually fixing

--- api.js
+++ api.js
@@ -1,1 +1,9 @@
-document.getElementById("homeButt").addEventListener("click",saveAuth);
+document.getElementById("apiSaveButt").addEventListener("click",saveAuth);
+
+function saveAuth() {
+    chrome.storage.local.set({ "civic": document.getElementById("civicKey").value, 
+    "gpt": document.getElementById("gptKey").value}).then(() => {
+        console.log("Civic Key is set");
+        console.log("GPT Key is set");
+    });
+}
--- storage.js
+++ storage.js
@@ -54,14 +54,6 @@ function retrieve() {
     }, 1000)
 }
 
-function saveAuth() {
-    chrome.storage.local.set({ "civic": document.getElementById("civicKey").value, 
-    "gpt": document.getElementById("gptKey").value}).then(() => {
-        console.log("Civic Key is set");
-        console.log("GPT Key is set");
-    });
-}
-
 function getAuth(x){
     y = "";
     if (x=="civic") {
