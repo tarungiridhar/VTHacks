83cb409a202d346b4263859ea1a9c99cb0b57137
tarungiridhar <tarungiri1237@gmail.com>
2023-09-16 20:39:02 -0400

    SOTRGE WORKS

--- storage.js
+++ storage.js
@@ -1,11 +1,17 @@
+document.getElementById("saveButt").addEventListener("click",store);
+
 function store() {
     chrome.storage.local.set({ "firstName": document.getElementById("FirstName").value }).then(() => {
         console.log("First Name is set");
       });
 
-    chrome.storage.local.get(["firstName"]).then((result) => {
-        console.log("Value currently is " + result.key);
-      });
+    /*chrome.storage.local.get(["firstName"]).then((result) => {
+        console.log("Value currently is " + result);
+      });*/
+    
+      chrome.storage.local.get(/* String or Array */["firstName"], function(items){
+        //  items = [ { "phasersTo": "awesome" } ]
+        console.log(items)
+    });
 }
 
-document.getElementById("saveButt").addEventListener("click",store());
