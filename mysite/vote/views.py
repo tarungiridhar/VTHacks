15d73624c20901c74a123b01a1d6853858435ed8
tarungiridhar <tarungiri1237@gmail.com>
2023-09-17 02:59:46 -0400

    Update views.py

--- mysite/vote/views.py
+++ mysite/vote/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+import json
+from django.contrib.auth.models import User #####
+from django.http import JsonResponse , HttpResponse ####
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the wiki index.")
+
+
+# https://pypi.org/project/wikipedia/#description
+def get_wiki_summary(request):
+    topic = request.GET.get('topic', None)
+
+    print('topic:', topic)
+
+    data = {
+        'summary': wikipedia.summary(topic, sentences=1),
+        'raw': 'Successful',
+    }
+
+    print('json-data to be sent: ', data)
+
+    return JsonResponse(data)
