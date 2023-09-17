05da5445f2297515206505d87a37b5cc32c1ba04
tarungiridhar <tarungiri1237@gmail.com>
2023-09-17 00:24:00 -0400

    Update storage.js

--- storage.js
+++ storage.js
@@ -47,52 +47,44 @@ function store() {
     });
 }
 
-async function retrieve() {
+function retrieve() {
+    x = "";
     
-    
-    const y = await getter();
+    getter().then(console.log("get after " + x));
 
-    async function getter() { 
-        x = "";
+    function getter() { 
         chrome.storage.local.get(["firstName"], function(items){
-            
             console.log("Value currently is " + items.firstName);
             document.getElementById("FirstName").value = items.firstName;
         });
         chrome.storage.local.get(["lastName"], function(items){
-            
             console.log("Value currently is " + items.lastName);
             document.getElementById("LastName").value = items.lastName;
         });
         chrome.storage.local.get(["address"], function(items){
-            
             console.log("Value currently is " + items.address);
             document.getElementById("Address").value = items.address;
             x += items.address;
         });
         chrome.storage.local.get(["city"], function(items){
-            
             console.log("Value currently is " + items.city);
             document.getElementById("City").value = items.city;
             x = x + " " + items.city;
         });
         chrome.storage.local.get(["stateNames"], function(items){
-            
             console.log("Value currently is " + items.stateNames);
             document.getElementById("StateNames").value = items.stateNames;
             x = x + ", " + items.stateNames;
         });
         chrome.storage.local.get(["zip"], function(items){
-            
             console.log("Value currently is " + items.zip);
             document.getElementById("Zip").value = items.zip;
             x = x + " " + items.zip;
         
         });
-        return x;
     }
     /*setTimeout(function(){
         console.log("get after " + x);
     }, 1000);*/
-    console.log("get after " + y);
+    console.log("get after " + x);
 }
