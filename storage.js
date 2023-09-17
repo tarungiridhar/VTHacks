211f34b9b95500f2fc487e1376dfe92a97de7509
tarungiridhar <tarungiri1237@gmail.com>
2023-09-16 21:15:46 -0400

    Update storage.js

--- storage.js
+++ storage.js
@@ -4,14 +4,45 @@ document.getElementById("retrieveButt").addEventListener("click",retrieve);
 function store() {
     chrome.storage.local.set({ "firstName": document.getElementById("FirstName").value }).then(() => {
         console.log("First Name is set");
-      });
+    });
+    
+    chrome.storage.local.set({ "lastName": document.getElementById("LastName").value }).then(() => {
+        console.log("Last Name is set");
+    });
 
-    /*chrome.storage.local.get(["firstName"]).then((result) => {
-        console.log("Value currently is " + result);
-      });*/
+    chrome.storage.local.set({ "address": document.getElementById("Address").value }).then(() => {
+        console.log("Address is set");
+    });
+
+    chrome.storage.local.set({ "city": document.getElementById("City").value }).then(() => {
+        console.log("City is set");
+    });
+
+    chrome.storage.local.set({ "zip": document.getElementById("Zip").value }).then(() => {
+        console.log("Zip is set");
+    });
+
+    chrome.storage.local.set({ "stateNames": document.getElementById("StateNames").value }).then(() => {
+        console.log("State Names is set");
+    });
     
     chrome.storage.local.get(["firstName"], function(items){
-        console.log(items)
+        console.log(items.firstName)
+    });
+    chrome.storage.local.get(["lastName"], function(items){
+        console.log(items.lastName)
+    });
+    chrome.storage.local.get(["address"], function(items){
+        console.log(items.address)
+    });
+    chrome.storage.local.get(["city"], function(items){
+        console.log(items.city)
+    });
+    chrome.storage.local.get(["zip"], function(items){
+        console.log(items.zip)
+    });
+    chrome.storage.local.get(["stateNames"], function(items){
+        console.log(items.stateNames)
     });
 }
 
@@ -19,5 +50,25 @@ function retrieve() {
     chrome.storage.local.get(["firstName"], x = function(items){
         console.log("Value currently is " + items.firstName);
         document.getElementById("FirstName").value = items.firstName;
+    });
+    chrome.storage.local.get(["lastName"], x = function(items){
+        console.log("Value currently is " + items.lastName);
+        document.getElementById("LastName").value = items.lastName;
+    });
+    chrome.storage.local.get(["address"], x = function(items){
+        console.log("Value currently is " + items.address);
+        document.getElementById("Address").value = items.address;
+    });
+    chrome.storage.local.get(["city"], x = function(items){
+        console.log("Value currently is " + items.city);
+        document.getElementById("City").value = items.city;
+    });
+    chrome.storage.local.get(["zip"], x = function(items){
+        console.log("Value currently is " + items.zip);
+        document.getElementById("Zip").value = items.zip;
+    });
+    chrome.storage.local.get(["stateNames"], x = function(items){
+        console.log("Value currently is " + items.stateNames);
+        document.getElementById("StateNames").value = items.stateNames;
     });
 }
