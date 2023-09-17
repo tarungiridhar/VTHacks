ba95481a366743d1ddfe961b76c4ac11e4842077
tziffer <118021530+tziffer@users.noreply.github.com>
2023-09-17 04:30:30 -0400

    Chilling

--- storage.js
+++ storage.js
@@ -1,4 +1,4 @@
-if (document.title == "startpage.html") {
+if (document.title == "startpage") {
     document.getElementById("saveButt").addEventListener("click",store);
     document.getElementById("nextButt").addEventListener("click",store);
     document.getElementById("retrieveButt").addEventListener("click",retrieve);
@@ -31,22 +31,28 @@ function retrieve() {
     x = "";
 
     chrome.storage.local.get(["firstName", "lastName", "address", "city", "zip", "stateNames"], function(items){
-        console.log(items.firstName)
-        document.getElementById("FirstName").value = items.firstName;
-        console.log(items.lastName)
-        document.getElementById("LastName").value = items.lastName;
-        console.log(items.address)
-        document.getElementById("Address").value = items.address;
+        if (document.title == "startpage") {
+            document.getElementById("StateNames").value = items.stateNames;
+            document.getElementById("FirstName").value = items.firstName;
+            document.getElementById("Zip").value = items.zip;
+            document.getElementById("LastName").value = items.lastName;
+            
+            document.getElementById("Address").value = items.address;
+            document.getElementById("City").value = items.city;
+            
+        }
         x += items.address;
         x = x.replaceAll(" ","%20")
+        console.log(items.firstName)
+        console.log(items.lastName)
+        console.log(items.address)
         console.log(items.city)
-        document.getElementById("City").value = items.city;
         x = x + "%20" + items.city;
         console.log(items.stateNames)
-        document.getElementById("StateNames").value = items.stateNames;
+        
         x = x + "%20" + items.stateNames;
         console.log(items.zip)
-        document.getElementById("Zip").value = items.zip;
+        
             x = x + "%20" + items.zip;
     });
 
